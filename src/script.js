3e9e36a085b3613f9c3508eee290621bf84f9987
Eduardo Daniel Cuomo <reduardo7@gmail.com>
2021-09-07 19:26:23 -0300

    fixes

--- src/script.js
+++ src/script.js
@@ -2,12 +2,8 @@
 
 (function (global) {
   var crypt = {
-    secret: function (pass = null) {
-      if (pass) {
-        return pass;
-      }
-
-      pass = prompt('Password?');
+    secret: function () {
+      var pass = prompt('Password?');
 
       if (!pass) {
         throw new Error('No password!');
@@ -26,30 +22,29 @@
 
     encrypt: function (clear, pass = null) {
       try {
-        var cipher = CryptoJS.AES.encrypt(clear, crypt.secret(pass));
+        var cipher = CryptoJS.AES.encrypt(clear, pass || crypt.secret());
         cipher = cipher.toString();
         return cipher;
       } catch (err) {
-        console.error(err);
         alert(err.message);
+        throw err;
       }
     },
 
     decrypt: function (cipher, pass = null) {
       try {
-        var decipher = CryptoJS.AES.decrypt(cipher, crypt.secret(pass));
+        var decipher = CryptoJS.AES.decrypt(cipher, pass || crypt.secret());
         decipher = decipher.toString(CryptoJS.enc.Utf8);
         return decipher;
       } catch (err) {
-        console.error(err);
         alert(err.message);
+        throw err;
       }
     }
   };
 
   function openDrive() {
     var url = crypt.decrypt('U2FsdGVkX18i0dSp8kZzgz9wAWiGdEjbVyM7Jtq8426QT1nSduDcCsfHifz9gnYPmKDb9R1sYvI7qm9pr6Uy+DPGb/L53AAU3JjNUydXqfP1fMaAhwpviBiGj/oRKBnPRuBnHZqa2gJxNyX8h+ohM8qTeq8kt8aAEBp5CQbjLnc=');
-
     window.open(url);
   }
 
